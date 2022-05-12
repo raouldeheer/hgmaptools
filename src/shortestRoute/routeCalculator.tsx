ecd3a399824bd63fd0f83c3f6f523588f19875be
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-12 23:16:34 +0200

    Added map

--- src/shortestRoute/routeCalculator.tsx
+++ src/shortestRoute/routeCalculator.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import battlefields from "./battlefields.json";
 import RoutePoint from "./routePoint";
 import "./routeCalculator.css";
+import RouteMap from "./routeMap";
 
 export interface RouteResult {
     path: string[];
@@ -19,30 +20,38 @@ const RouteCalculator = (): JSX.Element => {
         setAnswer(result);
     };
 
-    return <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <h1 className="title">City route</h1>
+    return <div className="total">
+        <div className="Calculator">
+            <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                <h1 className="title">City route</h1>
 
-        <label htmlFor="bftitle1">Choose starting city:</label><br />
-        <select className="input" id="bftitle1" {...register("bftitle1")}>
-            {battlefields.map(title => <option key={title + "1"} value={title}>{title}</option>)}
-        </select><br />
-        {errors.bftitle1 && <><span className="message">Starting city is required</span><br /></>}
+                <label htmlFor="bftitle1">Choose starting city:</label><br />
+                <select className="input" id="bftitle1" {...register("bftitle1")}>
+                    {battlefields.map(title => <option key={title + "1"} value={title}>{title}</option>)}
+                </select><br />
+                {errors.bftitle1 && <><span className="message">Starting city is required</span><br /></>}
 
-        <label htmlFor="bftitle2">Choose destination city:</label><br />
-        <select className="input" id="bftitle2" {...register("bftitle2")}>
-            {battlefields.map(title => <option key={title + "2"} value={title}>{title}</option>)}
-        </select><br />
-        {errors.bftitle2 && <><span className="message">Destination city is required</span><br /></>}
+                <label htmlFor="bftitle2">Choose destination city:</label><br />
+                <select className="input" id="bftitle2" {...register("bftitle2")}>
+                    {battlefields.map(title => <option key={title + "2"} value={title}>{title}</option>)}
+                </select><br />
+                {errors.bftitle2 && <><span className="message">Destination city is required</span><br /></>}
 
-        <input className="submit" type="submit" />
+                <input className="submit" type="submit" />
+            </form>
+            {answer ? <div className="pathlist">
+                <h2>Route</h2>
+                <p>Distance: {answer.distance}</p>
+                <p>Path:</p>
+                <ul>{answer.path.map(v => <RoutePoint key={v} id={v} />)}</ul>
+            </div> : null}
+        </div>
+        <div className="Map">
+            <RouteMap />
+        </div>
 
-        {answer ? <>
-            <h2>Route</h2>
-            <p>Distance: {answer.distance}</p>
-            <p>Path:</p>
-            <ul>{answer.path.map(v => <RoutePoint key={v} id={v} />)}</ul>
-        </> : null}
-    </form>;
+
+    </div>;
 };
 
 export default RouteCalculator;
---
+++ src/shortestRoute/routeMap.tsx
@@ -0,0 +1,14 @@
+import image from "hagcp-assets/images/background.png";
+import { useEffect, useState } from "react";
+
+const RouteMap = (): JSX.Element => {
+
+
+    return <div className="routeMap">
+
+        <img src={image} className="image" alt="background map" />
+        
+    </div>;
+};
+
+export default RouteMap;
