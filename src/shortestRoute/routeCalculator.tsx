e72eb24624a47acbfdaf425c0c45cb4a85c7d171
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-12 20:33:45 +0200

    Added routeCalculator logic

--- src/shortestRoute/routeCalculator.tsx
+++ src/shortestRoute/routeCalculator.tsx
@@ -1,85 +1,46 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import battlefields from "./battlefields.json";
+import RoutePoint from "./routePoint";
+import "./routeCalculator.css";
 
-const formStyle: React.CSSProperties = {
-    background: "#FFFFFF",
-    padding: "45px",
-    textAlign: "center",
-    width: "fit-content",
-};
-
-const inputStyle: React.CSSProperties = {
-    fontFamily: "sans-serif",
-    outline: 0,
-    background: "#f2f2f2",
-    border: 0,
-    margin: "15px 0 0",
-    padding: "15px",
-    boxSizing: "border-box",
-    fontSize: "14px",
-};
-
-const submitStyle: React.CSSProperties = {
-    marginTop: "15px",
-    fontFamily: "sans-serif",
-    textTransform: "uppercase",
-    outline: 0,
-    width: "100%",
-    background: "#4CAF50",
-    border: 0,
-    padding: "15px",
-    color: "#FFFFFF",
-    fontSize: "14px",
-    transition: "all 0.3 ease",
-    cursor: "pointer",
-};
-
-const TitleStyle = {
-    margin: "0 0 15px",
-};
-
-const messageStyle = {
-    margin: "0 0 15px",
-    color: "#b3b3b3",
-    fontSize: "12px",
-};
+export interface RouteResult {
+    path: string[];
+    distance: number;
+}
 
 const RouteCalculator = (): JSX.Element => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const [answer, setAnswer] = useState(null);
+    const [answer, setAnswer] = useState<RouteResult | null>(null);
 
-    const onSubmit = (data: any) => {
-        setAnswer(data);
-        // TODO do submit.
+    const onSubmit = async (data: any) => {
+        const result = await fetch(`https://hgwarmap.dphs.nl/api/battlefieldroute?bftitle1=${data.bftitle1}&bftitle2=${data.bftitle2}`)
+            .then(response => response.json() as unknown as RouteResult | null);
+        setAnswer(result);
     };
 
-    return <form style={formStyle} onSubmit={handleSubmit(onSubmit)}>
-        <h1 style={TitleStyle}>City route</h1>
+    return <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="title">City route</h1>
 
-        <label htmlFor="City1">Choose starting city:</label><br />
-        <select id="City1" {...register("City1")}>
-            <option value="volvo">Volvo</option>
-            {
-                // TODO add all city names 
-            }
+        <label htmlFor="bftitle1">Choose starting city:</label><br />
+        <select className="input" id="bftitle1" {...register("bftitle1")}>
+            {battlefields.map(title => <option key={title + "1"} value={title}>{title}</option>)}
         </select><br />
-        {errors.City1 && <><span style={messageStyle}>City1 is required</span><br /></>}
-        
-        <label htmlFor="City2">Choose starting city:</label><br />
-        <select id="City2" {...register("City2")}>
-            <option value="volvo">Volvo</option>
-            {
-                // TODO add all city names 
-            }
-        </select><br />
-        {errors.City2 && <><span style={messageStyle}>City2 is required</span><br /></>}
+        {errors.bftitle1 && <><span className="message">Starting city is required</span><br /></>}
 
-        <input style={submitStyle} type="submit" />
+        <label htmlFor="bftitle2">Choose destination city:</label><br />
+        <select className="input" id="bftitle2" {...register("bftitle2")}>
+            {battlefields.map(title => <option key={title + "2"} value={title}>{title}</option>)}
+        </select><br />
+        {errors.bftitle2 && <><span className="message">Destination city is required</span><br /></>}
+
+        <input className="submit" type="submit" />
 
         {answer ? <>
-            <p>{answer["City1"]}</p>
-            <p>{answer["City2"]}</p>
-            <p>{JSON.stringify(answer)}</p>
+            <h2>Route</h2>
+            <p>Distance: {answer.distance}</p>
+            <p>Path:</p>
+            <ul>{answer.path.map(v => <RoutePoint key={v} id={v} />)}</ul>
         </> : null}
     </form>;
 };
---
+++ src/shortestRoute/routePoint.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const RoutePoint = ({ id }: { id: string; }): JSX.Element => {
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        fetch(`https://hgwarmap.dphs.nl/api/battlefield?id=${id}`)
+            .then(data => data.json())
+            .then(data => {
+                if (data?.bftitle) setName(data.bftitle);
+            });
+    }, [id]);
+
+    return <li>{name}</li>;
+};
+
+export default RoutePoint;
