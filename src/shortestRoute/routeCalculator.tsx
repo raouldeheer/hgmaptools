eedee1d9f53e34d21c4ff4096ac16a01b606539f
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-19 11:44:08 +0200

    Added traveltime

--- src/shortestRoute/routeCalculator.tsx
+++ src/shortestRoute/routeCalculator.tsx
@@ -4,6 +4,13 @@ import battlefields from "./battlefields.json";
 import RoutePoint from "./routePoint";
 import "./routeCalculator.css";
 import RouteMap from "./routeMap";
+import commandnodetemplate from "hagcp-assets/json/commandnodetemplate.json";
+
+const commandnodetemplateNameToSpeed = new Map<string, number>();
+
+commandnodetemplate.forEach(element => {
+    commandnodetemplateNameToSpeed.set(element.name, element.speed);
+});
 
 export interface RouteResult {
     path: string[];
@@ -13,8 +20,10 @@ export interface RouteResult {
 const RouteCalculator = (): JSX.Element => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const [answer, setAnswer] = useState<RouteResult | null>(null);
+    const [ATType, setATType] = useState<string | null>(null);
 
     const onSubmit = async (data: any) => {
+        setATType(data.unit);
         const result = await fetch(`https://hgwarmap.dphs.nl/api/battlefieldroute?bftitle1=${data.bftitle1}&bftitle2=${data.bftitle2}`)
             .then(response => response.json() as unknown as RouteResult | null);
         setAnswer(result);
@@ -37,11 +46,21 @@ const RouteCalculator = (): JSX.Element => {
                 </select><br />
                 {errors.bftitle2 && <><span className="message">Destination city is required</span><br /></>}
 
+                <label htmlFor="unit">Choose assault team type:</label><br />
+                <select className="input" id="unit" {...register("unit")}>
+                    {Array.from(commandnodetemplateNameToSpeed.keys()).map(title => <option key={title} value={title}>{title}</option>)}
+                </select><br />
+                {errors.bftitle2 && <><span className="message">Destination city is required</span><br /></>}
+
                 <input className="submit" type="submit" />
             </form>
             {answer ? <div className="pathlist">
                 <h2>Route</h2>
-                <p>Distance: {answer.distance}</p>
+                <p>Distance: {answer.distance.toFixed(2)}</p>
+                {ATType ? <>
+                    <p>Traveltime grey: {(answer.distance / commandnodetemplateNameToSpeed.get(ATType)! * 1.0).toFixed(2)} Seconds</p>
+                    <p>Traveltime green: {(answer.distance / commandnodetemplateNameToSpeed.get(ATType)! * 1.0).toFixed(2)} Seconds</p>
+                </> : null}
                 <p>Path:</p>
                 <ul>{answer.path.map(v => <RoutePoint key={v} id={v} />)}</ul>
             </div> : null}
