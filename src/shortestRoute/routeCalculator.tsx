23abde91e480087c45f7daf550693bda591375f9
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-05-19 15:34:57 +0200

    Added airform

--- src/shortestRoute/routeCalculator.tsx
+++ src/shortestRoute/routeCalculator.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import battlefields from "./battlefields.json";
+import airfields from "./airfields.json";
 import RoutePoint from "./routePoint";
 import "./routeCalculator.css";
 import RouteMap from "./routeMap";
 import commandnodetemplate from "hagcp-assets/json/commandnodetemplate.json";
 
 const commandnodetemplateNameToSpeed = new Map<string, number>(commandnodetemplate.map(e => ([e.name, e.speed])));
+const planes = new Map<string, { speed: number, transportradius: number; }>(commandnodetemplate
+    .filter(e => e.transportradius > 0)
+    .map(e => ([e.name, { speed: e.speed, transportradius: e.transportradius }])));
 
 export interface RouteResult {
     path: string[];
@@ -17,6 +21,7 @@ const RouteCalculator = (): JSX.Element => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const [answer, setAnswer] = useState<RouteResult | null>(null);
     const [ATType, setATType] = useState<string | null>(null);
+    const [formType, setFormType] = useState("Ground");
 
     const onSubmit = async (data: any) => {
         setATType(data.unit);
@@ -25,31 +30,72 @@ const RouteCalculator = (): JSX.Element => {
         setAnswer(result);
     };
 
+    const onSubmitFly = async (data: any) => {
+        setATType(data.unit);
+        const result = await fetch(`https://hgwarmap.dphs.nl/api/battlefieldroute?bftitle1=${data.bftitle1}&bftitle2=${data.bftitle2}`)
+            .then(response => response.json() as unknown as RouteResult | null);
+        setAnswer(result);
+    };
+
+    const onChange = (e: { target: { value: string; }; }) => {
+        setFormType(e.target.value);
+        console.log(e.target.value);
+    };
+
     return <div className="total">
         <div className="Calculator">
-            <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                <h1 className="title">City route</h1>
+            <div className="form">
+                <select className="input" name="select" onChange={onChange}>
+                    <option>Ground</option>
+                    <option>Air</option>
+                </select>
+                {formType === "Ground" ? <form onSubmit={handleSubmit(onSubmit)}>
+                    <h1 className="title">City route</h1>
 
-                <label htmlFor="bftitle1">Choose starting city:</label><br />
-                <select className="input" id="bftitle1" {...register("bftitle1")}>
-                    {battlefields.map(title => <option key={title + "1"} value={title}>{title}</option>)}
-                </select><br />
-                {errors.bftitle1 && <><span className="message">Starting city is required</span><br /></>}
+                    <label htmlFor="bftitle1">Choose starting city:</label><br />
+                    <select className="input" id="bftitle1" {...register("bftitle1")}>
+                        {battlefields.map(title => <option key={title + "1"} value={title}>{title}</option>)}
+                    </select><br />
+                    {errors.bftitle1 && <><span className="message">Starting city is required</span><br /></>}
 
-                <label htmlFor="bftitle2">Choose destination city:</label><br />
-                <select className="input" id="bftitle2" {...register("bftitle2")}>
-                    {battlefields.map(title => <option key={title + "2"} value={title}>{title}</option>)}
-                </select><br />
-                {errors.bftitle2 && <><span className="message">Destination city is required</span><br /></>}
+                    <label htmlFor="bftitle2">Choose destination city:</label><br />
+                    <select className="input" id="bftitle2" {...register("bftitle2")}>
+                        {battlefields.map(title => <option key={title + "2"} value={title}>{title}</option>)}
+                    </select><br />
+                    {errors.bftitle2 && <><span className="message">Destination city is required</span><br /></>}
 
-                <label htmlFor="unit">Choose assault team type:</label><br />
-                <select className="input" id="unit" {...register("unit")}>
-                    {Array.from(commandnodetemplateNameToSpeed.keys()).map(title => <option key={title} value={title}>{title}</option>)}
-                </select><br />
-                {errors.bftitle2 && <><span className="message">Destination city is required</span><br /></>}
+                    <label htmlFor="unit">Choose assault team type:</label><br />
+                    <select className="input" id="unit" {...register("unit")}>
+                        {Array.from(commandnodetemplateNameToSpeed.keys()).map(title => <option key={title} value={title}>{title}</option>)}
+                    </select><br />
+                    {errors.unit && <><span className="message">Assault team is required</span><br /></>}
 
-                <input className="submit" type="submit" />
-            </form>
+                    <input className="submit" type="submit" />
+                </form> : null}
+                {formType === "Air" ? <form onSubmit={handleSubmit(onSubmitFly)}>
+                    <h1 className="title">City route</h1>
+
+                    <label htmlFor="bftitle1">Choose starting airfield:</label><br />
+                    <select className="input" id="bftitle1" {...register("bftitle1")}>
+                        {airfields.map(title => <option key={title + "1"} value={title}>{title}</option>)}
+                    </select><br />
+                    {errors.bftitle1 && <><span className="message">Starting airfield is required</span><br /></>}
+
+                    <label htmlFor="bftitle2">Choose destination airfield:</label><br />
+                    <select className="input" id="bftitle2" {...register("bftitle2")}>
+                        {airfields.map(title => <option key={title + "2"} value={title}>{title}</option>)}
+                    </select><br />
+                    {errors.bftitle2 && <><span className="message">Destination airfield is required</span><br /></>}
+
+                    <label htmlFor="unit">Choose assault team type:</label><br />
+                    <select className="input" id="unit" {...register("unit")}>
+                        {Array.from(planes.keys()).map(title => <option key={title} value={title}>{title}</option>)}
+                    </select><br />
+                    {errors.unit && <><span className="message">Assault team is required</span><br /></>}
+
+                    <input className="submit" type="submit" />
+                </form> : null}
+            </div>
             {answer ? <div className="pathlist">
                 <h2>Route</h2>
                 <p>Distance: {answer.distance.toFixed(2)}</p>
